b92283b38719c9c4e66cbd7e597e69b6f64ad8a3
Maks6831 <ma.khan6971@gmail.com>
2023-08-29 13:00:35 +0100

    findIndex, findLast, findLastIndex, flat and flatmap done

--- Methods.js
+++ Methods.js
@@ -28,13 +28,20 @@ const findFunction = (element) => {
     return element > 20;
 }
 
+const findFunctionTwo = (element) => {
+    return element > 100;
+}
+
+
 
 const methodObject = {
     fruits : ["apple", "banana", "orange", "grape"],
     numbers : ["1","2","3","4","5","6","7"],
     letters : ["a","b","c","d","e","f","g"],
-    realNumbers : [1,2,3,4,5,6,7,8,9,10, 25],
+    realNumbers : [1,2,3,4,5,6,7,8,9,10, 25, 30],
     mixedArray : ["1","2","3",6,7,8,9,"banana", "orange", "grape"],
+    subArrayOne: [[1,2],[3,4],[5,6]],
+    subArrayTwo: [1, 2, [3, [4, 5, 6], 7], 8],
     emptyArray : [],
     newAt : function(index){
         return this.fruits[index]
@@ -108,7 +115,76 @@ const methodObject = {
 
         return null
 
+    },
+    newFindIndex : function(funct, array){
+        let counter = 0
+        for(let i = 0; i < array.length; i++){
+            if(funct(array[i])){   
+                counter++
+                return i;   
+            }
+            
+        }
+        if(!counter){
+            return -1;
+        }
+      
+    },
+    newFindLast : function(funct, array){
+        lastNumber = 0;
+        for(let i = 0; i < array.length; i++){
+            if(funct(array[i])){   
+                lastNumber = array[i];  
+            }
+        
     }
+    return lastNumber;
+    },
+    newFindLastIndex : function(funct, array){
+        let lastIndex = 0
+        for(let i = 0; i < array.length; i++){
+            if(funct(array[i])){   
+                lastIndex = i;  
+            }     
+    }
+    return lastIndex
+    },
+    newFlat: function(array){
+        newArr = [];
+        for(let i = 0; i < array.length; i++){
+            if(Array.isArray(array[i])){
+                for(let j = 0; j < array[i].length; j++){
+                    if(Array.isArray(array[i][j])){
+                        for(let k = 0; k < array[i][j].length; k++){
+                            newArr.push(array[i][j][k])
+                        }
+                        
+                    } else {
+                        newArr.push(array[i][j])
+                    }               
+                } 
+            } else {
+                newArr.push(array[i])
+            }
+        }
+        return newArr;
+    },
+    newFlatMap: function(funct, array){
+        let newArr = [];
+        for(let i = 0; i < array.length; i++){
+            newArr.push(funct(array[i]))
+        }
+
+        return newArr;
+    },
+    newForEach : function (funct, array){
+        let count =0 
+        while(count < array.length ){
+            return funct(array[count])
+            count++
+        }
+    }
+
 }
 
 
@@ -146,5 +222,36 @@ const methodObject = {
 // find
 
 //console.log(methodObject.newFind(filterFunctionTwo, methodObject.realNumbers));// returns 1
-console.log(methodObject.newFind(findFunction, methodObject.realNumbers));// returns 25
+//console.log(methodObject.newFind(findFunction, methodObject.realNumbers));// returns 25
 
+
+// findIndex
+
+//console.log(methodObject.newFindIndex(findFunction, methodObject.realNumbers)) // outputs 10
+//console.log(methodObject.newFindIndex(findFunctionTwo, methodObject.realNumbers)) // outputs -1
+
+
+// findLast
+
+//console.log(methodObject.newFindLast(findFunction, methodObject.realNumbers)) // outputs 30
+
+// findLastIndex
+
+//console.log(methodObject.newFindLastIndex(findFunction, methodObject.realNumbers)) // outputs 11
+
+
+// flat
+
+//console.log(methodObject.newFlat( methodObject.subArrayOne)) // outputs [ 1, 2, 3, 4, 5, 6 ]
+//console.log(methodObject.newFlat( methodObject.subArrayTwo)) // outputs [  1, 2, 3, 4, 5, 6, 7, 8 ]
+
+
+// flatMap
+
+//console.log(methodObject.newFlatMap((x) => x * 2 , methodObject.realNumbers)) // outputs [2,  4,  6,  8, 10, 12, 14, 16, 18, 20,50, 60]
+
+
+// forEach 
+
+
+console.log(methodObject.newForEach((x) => x * 2 , methodObject.realNumbers))
